e50fe1a5e0f549aa2cee8c52d07596b4786e0a05
Firewine <1451661318@qq.com>
2018-11-09 14:25:52 +0800

    spring的学习

---
+++ day1_spring1/src/com/spring/ioc1/YelloMouseWolf.java
@@ -0,0 +1,21 @@
+package com.spring.ioc1;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @version : 1.0
+ * @auther : Firewine
+ * @mail ： 1451661318@qq.com
+ * @Program Name: <br>
+ * @Create : 2018-11-08-20:14
+ */
+public class YelloMouseWolf {
+
+    public YelloMouseWolf(){
+        System.out.println("YelloMouseWolf.........");
+    }
+
+    public void bahavior(){
+        System.out.println("偷鸡");
+    }
+}
