1d89d95c890de5b0627973f384a9e3e2a08d7967
XYJ <1451661318@qq.com>
2018-12-18 21:55:26 +0800

    类型转换器

---
+++ day5_springmvc2/src/com/springmvc/crud/handlers/EmployeeHandler.java
@@ -0,0 +1,68 @@
+package com.springmvc.crud.handlers;
+
+import com.springmvc.crud.dao.DepartmentDao;
+import com.springmvc.crud.dao.EmployeeDao;
+import com.springmvc.crud.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@Controller
+public class EmployeeHandler {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @ModelAttribute
+    public void getEmployee(@RequestParam(value="id",required=false) Integer id,
+                            Map<String, Object> map){
+        if(id != null){
+            map.put("employee", employeeDao.get(id));
+        }
+    }
+
+    @RequestMapping(value="/emp", method=RequestMethod.PUT)
+    public String update(Employee employee){
+        employeeDao.save(employee);
+
+        return "redirect:/emps";
+    }
+    @RequestMapping(value="/emp/{id}", method=RequestMethod.GET)
+    public String input(@PathVariable("id") Integer id, Map<String, Object> map){
+        map.put("employee", employeeDao.get(id));
+        map.put("departments", departmentDao.getDepartments());
+        return "input";
+    }
+    @RequestMapping(value = "/emp/{id}",method = RequestMethod.DELETE)
+    public String delete(@PathVariable Integer id){
+
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
+    @RequestMapping(value = "/emp",method = RequestMethod.POST)
+    public String save(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+    @RequestMapping(value = "emp" ,method = RequestMethod.GET)
+    public String input(Map<String,Object> map){
+
+        map.put("departments",departmentDao.getDepartments());
+        map.put("employee",new Employee());
+        return "input";
+    }
+
+    @RequestMapping("/emps")
+    public String list(Map<String,Object> map){
+
+        map.put("employee",employeeDao.getAll());
+
+        return "list";
+    }
+}
---
+++ day5_springmvc2/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xyjwork
+  Date: 2018/12/18
+  Time: 14:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+    <a href="emps">display all Employee</a>
+  </body>
+</html>
